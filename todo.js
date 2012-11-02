b9a3f0aaf11d8bc429c035a249851de870ae69a6
Luis Sotomayor <luis.sotomayor@gmail.com>
2012-11-02 11:41:34 -0700

    Completed TODOs part of the tutorial.

--- todo.js
+++ todo.js
@@ -2,7 +2,9 @@
     'use strict';
 
     app.TodoController = function($scope) {
-        $scope.totalTodos = 4;
+        $scope.totalTodos = function() {
+            return $scope.todos.length;
+        };
 
         $scope.todos = [{
             text: 'foo',
@@ -12,6 +14,26 @@
             text: 'bar',
             done: false
         }];
+
+        $scope.addTodo = function() {
+            var newTodoText = $scope.newTodoText;
+            if (!newTodoText) {
+                return;
+            }
+
+            $scope.todos.push({
+                text: newTodoText,
+                done: false
+            });
+
+            $scope.newTodoText = '';
+        };
+
+        $scope.clearDone = function() {
+            $scope.todos = $scope.todos.filter(function(todo) {
+                return ! todo.done;
+            });
+        };
     };
 } (window.app));
 
